9180dd1c9f2445aad2d8db197d2c0f1458a7786f
tamgtasa2611 <tamgtasa2611@gmail.com>
2024-01-23 11:47:13 +0700

    laravel - v1
    file mac dinh

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@vite(["resources/sass/app.scss"])
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <th class="text-danger">1</th>
+        <th class="text-success">2</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td class="bg-warning">A</td>
+        <td class="bg-light">B</td>
+    </tr>
+    </tbody>
+</table>
+
