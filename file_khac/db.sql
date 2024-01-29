1532ccd03d490a0d1d7c55a9e58aa1f326e99fef
tamgtasa2611 <tamgtasa2611@gmail.com>
2024-01-29 20:34:55 +0700

    update db

--- file_khac/db.sql
+++ file_khac/db.sql
@@ -1,95 +1,92 @@
-CREATE DATABASE perfumestore;
-USE perfumestore;
+CREATE DATABASE sanninstore;
+USE sanninstore;
 
 CREATE TABLE brands (
-	id INT auto_increment,
+	brand_id INT auto_increment,
     brand_name VARCHAR(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (brand_id)
 );
 
-CREATE TABLE genders (
-	id INT auto_increment,
-    gender_name VARCHAR(13),
-    PRIMARY KEY (id)
+CREATE TABLE ages (
+	age_id INT auto_increment,
+    age_name VARCHAR(13),
+    description TEXT,
+    PRIMARY KEY (age_id)
 );
 
-CREATE TABLE product_types (
-	id INT auto_increment,
-    type_name VARCHAR(100),
-    PRIMARY KEY (id)
+CREATE TABLE categories (
+	category_id INT auto_increment,
+    category_name VARCHAR(100),
+    description TEXT,
+    PRIMARY KEY (category_id)
 );
 
 CREATE TABLE countries (
-	id INT auto_increment,
+	country_id INT auto_increment,
     country_name VARCHAR(100),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE sizes (
-	id INT auto_increment,
-    size INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (country_id)
 );
 
 CREATE TABLE products (
-	id INT auto_increment,
+	product_id INT auto_increment,
     product_name VARCHAR(255),
     quantity INT,
     price FLOAT,
     description TEXT,
     image TEXT,
-    size_id INT,
-    type_id INT,
+    category_id INT,
     country_id INT,
-    gender_id INT,
+    age_id INT,
     brand_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (size_id) REFERENCES sizes(id),
-    FOREIGN KEY (type_id) REFERENCES product_types(id),
-    FOREIGN KEY (country_id) REFERENCES countries(id),
-    FOREIGN KEY (gender_id) REFERENCES genders(id),
-    FOREIGN KEY (brand_id) REFERENCES brands(id)
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (country_id) REFERENCES countries(country_id),
+    FOREIGN KEY (age_id) REFERENCES ages(age_id),
+    FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
 );
 
 CREATE TABLE admins (
-	id INT auto_increment,
+	admin_id INT auto_increment,
     email VARCHAR(255) unique,
     password VARCHAR(255),
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     phone_number VARCHAR(13),
     level INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (admin_id)
 );
 
 CREATE TABLE customers (
-	id INT auto_increment,
+	customer_id INT auto_increment,
     email VARCHAR(255) unique,
     password VARCHAR(255),
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     phone_number VARCHAR(13),
     address TEXT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (customer_id)
 );
 
 CREATE TABLE payment_methods (
-	id INT auto_increment,
+	method_id INT auto_increment,
     method_name VARCHAR(100),
-    PRIMARY KEY (id)
+    PRIMARY KEY (method_id)
 );
 
 CREATE TABLE orders (
-	id INT auto_increment,
+	order_id INT auto_increment,
     order_date DATETIME,
     order_status INT,
+    receiver_name VARCHAR(255),
+    receiver_phone VARCHAR(13),
+    receiver_address TEXT,
     admin_id INT,
     customer_id INT,
     method_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (admin_id) REFERENCES admins(id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id),
-    FOREIGN KEY (method_id) REFERENCES payment_methods(id)
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (admin_id) REFERENCES admins(admin_id),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (method_id) REFERENCES payment_methods(method_id)
 );
 
 CREATE TABLE orders_details (
@@ -98,44 +95,48 @@ CREATE TABLE orders_details (
     sold_price FLOAT,
     sold_quantity INT,
     PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
 /*================================================================*/
 
 INSERT INTO brands(brand_name) VALUES
-("Dior"),
-("Chanel"),
-("D&G");
+("Mojang Studios"),
+("Disney"),
+("Marvel"),
+("DC"),
+("Sega"),
+("Nintendo");
 
-INSERT INTO genders(gender_name) VALUES
-("Male"),
-("Female"),
-("Unisex");
+INSERT INTO ages(age_name) VALUES
+("1.5+"),
+("4+"),
+("6+"),
+("9+"),
+("13+"),
+("18+");
 
-INSERT INTO product_types(type_name) VALUES
-("Common"),
-("Unique");
+INSERT INTO categories(category_name) VALUES
+("Batman"),
+("Minecraft"),
+("Lunar New Year"),
+("Valentine"),
+("Disney"),
+("Marvel");
 
 INSERT INTO countries(country_name) VALUES
 ("United States"),
 ("Korea"),
 ("Italy"),
 ("China"),
-("Japan");
+("Japan"),
+("Vietnam");
 
-INSERT INTO sizes(size) VALUES
-(30),
-(50),
-(100),
-(125);
-
-INSERT INTO products(product_name, quantity, price, description, image, size_id, type_id, country_id, gender_id, brand_id) VALUES
-("Product 1", 10, 49.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 1, 1, 1, 1, 1),
-("Product 2", 10, 69.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 2, 2, 2, 2, 2),
-("Product 3", 10, 100.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 3, 1, 1, 3, 2),
-("Product 4", 10, 89.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 4, 1, 3, 3, 1),
-("Product 5", 10, 79.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 1, 2, 1, 1, 1),
-("Product 6", 10, 29.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 2, 1, 4, 3, 3),
-("Product 7", 10, 159.99, "", "https://mediacdn.livestory.io/v1/armani/posts/r1000/6349244778f58e000b07e6a8.png.webp", 3, 2, 1, 2, 2);
+INSERT INTO products(product_name, quantity, price, description, image, category_id, country_id, age_id, brand_id) VALUES
+("Auspicious Dragon", 100, 89.99, "", "images/products/product_1.webp", 3, 1, 6, 2),
+("Batmobile™: Batman™ vs. The Joker™ Chase", 100, 47.99, "", "images/products/product_2.webp", 1, 2, 3, 4),
+("Spring Festival Mickey Mouse", 100, 9.99, "", "images/products/product_3.webp", 5, 3, 1, 2),
+("Nano Gauntlet", 100, 69.99, "", "images/products/product_4.webp", 3, 4, 2, 6),
+("The Abandoned Mine", 100, 19.99, "", "images/products/product_5.webp", 2, 5, 4, 1),
+("Heart Ornament", 100, 12.99, "", "images/products/product_6.webp", 4, 6, 5, 5);
