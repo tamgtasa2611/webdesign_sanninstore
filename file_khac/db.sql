2f36079557edfec0b20c6b28394a9ceefb5a4efa
tamgtasa2611 <tamgtasa2611@gmail.com>
2024-01-30 10:25:47 +0700

    db

--- file_khac/db.sql
+++ file_khac/db.sql
@@ -2,33 +2,33 @@ CREATE DATABASE sanninstore;
 USE sanninstore;
 
 CREATE TABLE brands (
-	brand_id INT auto_increment,
+	id INT auto_increment,
     brand_name VARCHAR(255),
-    PRIMARY KEY (brand_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE ages (
-	age_id INT auto_increment,
+	id INT auto_increment,
     age_name VARCHAR(13),
     description TEXT,
-    PRIMARY KEY (age_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE categories (
-	category_id INT auto_increment,
+	id INT auto_increment,
     category_name VARCHAR(100),
     description TEXT,
-    PRIMARY KEY (category_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE countries (
-	country_id INT auto_increment,
+	id INT auto_increment,
     country_name VARCHAR(100),
-    PRIMARY KEY (country_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE products (
-	product_id INT auto_increment,
+	id INT auto_increment,
     product_name VARCHAR(255),
     quantity INT,
     price FLOAT,
@@ -38,43 +38,43 @@ CREATE TABLE products (
     country_id INT,
     age_id INT,
     brand_id INT,
-    PRIMARY KEY (product_id),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (country_id) REFERENCES countries(country_id),
-    FOREIGN KEY (age_id) REFERENCES ages(age_id),
-    FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (country_id) REFERENCES countries(id),
+    FOREIGN KEY (age_id) REFERENCES ages(id),
+    FOREIGN KEY (brand_id) REFERENCES brands(id)
 );
 
 CREATE TABLE admins (
-	admin_id INT auto_increment,
+	id INT auto_increment,
     email VARCHAR(255) unique,
     password VARCHAR(255),
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     phone_number VARCHAR(13),
     level INT,
-    PRIMARY KEY (admin_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE customers (
-	customer_id INT auto_increment,
+	id INT auto_increment,
     email VARCHAR(255) unique,
     password VARCHAR(255),
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     phone_number VARCHAR(13),
     address TEXT,
-    PRIMARY KEY (customer_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE payment_methods (
-	method_id INT auto_increment,
+	id INT auto_increment,
     method_name VARCHAR(100),
-    PRIMARY KEY (method_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE orders (
-	order_id INT auto_increment,
+	id INT auto_increment,
     order_date DATETIME,
     order_status INT,
     receiver_name VARCHAR(255),
@@ -83,10 +83,10 @@ CREATE TABLE orders (
     admin_id INT,
     customer_id INT,
     method_id INT,
-    PRIMARY KEY (order_id),
-    FOREIGN KEY (admin_id) REFERENCES admins(admin_id),
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-    FOREIGN KEY (method_id) REFERENCES payment_methods(method_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (admin_id) REFERENCES admins(id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (method_id) REFERENCES payment_methods(id)
 );
 
 CREATE TABLE orders_details (
@@ -95,8 +95,8 @@ CREATE TABLE orders_details (
     sold_price FLOAT,
     sold_quantity INT,
     PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 /*================================================================*/
@@ -134,9 +134,9 @@ INSERT INTO countries(country_name) VALUES
 ("Vietnam");
 
 INSERT INTO products(product_name, quantity, price, description, image, category_id, country_id, age_id, brand_id) VALUES
-("Auspicious Dragon", 100, 89.99, "", "images/products/product_1.webp", 3, 1, 6, 2),
-("Batmobile™: Batman™ vs. The Joker™ Chase", 100, 47.99, "", "images/products/product_2.webp", 1, 2, 3, 4),
-("Spring Festival Mickey Mouse", 100, 9.99, "", "images/products/product_3.webp", 5, 3, 1, 2),
-("Nano Gauntlet", 100, 69.99, "", "images/products/product_4.webp", 3, 4, 2, 6),
-("The Abandoned Mine", 100, 19.99, "", "images/products/product_5.webp", 2, 5, 4, 1),
-("Heart Ornament", 100, 12.99, "", "images/products/product_6.webp", 4, 6, 5, 5);
+("2Auspicious Dragon", 100, 89.99, "", "images/products/product_1.webp", 3, 1, 6, 2),
+("2Batmobile™: Batman™ vs. The Joker™ Chase", 100, 47.99, "", "images/products/product_2.webp", 1, 2, 3, 4),
+("2Spring Festival Mickey Mouse", 100, 9.99, "", "images/products/product_3.webp", 5, 3, 1, 2),
+("2Nano Gauntlet", 100, 69.99, "", "images/products/product_4.webp", 3, 4, 2, 6),
+("2The Abandoned Mine", 100, 19.99, "", "images/products/product_5.webp", 2, 5, 4, 1),
+("2Heart Ornament", 100, 12.99, "", "images/products/product_6.webp", 4, 6, 5, 5);
